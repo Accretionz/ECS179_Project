52f0ffcd6614a1a0169758db5e79a5071a018475
dan-medina <dmedinamail@gmail.com>
2024-03-05 00:23:22 -0800

    implemented map with terrain objects

--- Hollow/Assets/Scripts/PlayerController.cs
+++ Hollow/Assets/Scripts/PlayerController.cs
@@ -1,53 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Player.Command;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] private float speed = 5.0f;
+    [SerializeField] public float speed = 20.0f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
 
-    // private IPlayerCommand right;
-    // private IPlayerCommand left;
-    // private IPlayerCommand up;
-    // private IPlayerCommand down;
-    void Start()
+    void Awake()
     {
-        // this.right = ScriptableObject.CreateInstance<MovePlayerRight>();
-        // this.left = ScriptableObject.CreateInstance<MovePlayerLeft>();
-        // this.up = ScriptableObject.CreateInstance<MovePlayerUp>();
-        // this.down = ScriptableObject.CreateInstance<MovePlayerDown>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        if(!rb)
+        {
+            Debug.LogError("No rigid body detected");
+        }
+       
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        // if (Input.GetAxis("Horizontal") > 0.01)
-        // {
-        //     this.right.Execute(this.gameObject);
-        // }
-        // if (Input.GetAxis("Horizontal") < -0.01)
-        // {
-        //     this.left.Execute(this.gameObject);
-        // }
-        // if (Input.GetAxis("Vertical") > 0.01)
-        // {
-        //     this.up.Execute(this.gameObject);
-        // }
-        // if (Input.GetAxis("Vertical") < -0.01)
-        // {
-        //     this.down.Execute(this.gameObject);
-        // }
-        if (Input.GetAxis("Horizontal") > 0.01)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        }
-        if (Input.GetAxis("Horizontal") < -0.01)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-        }
-        var movementDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
-        gameObject.transform.Translate(movementDirection * Time.deltaTime * speed);
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+       
+        moveInput.Normalize();
+
+        rb.velocity = moveInput * speed;
+        
+
     }
 }
