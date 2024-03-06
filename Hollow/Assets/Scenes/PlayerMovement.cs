4a19ba1625e8c1709f4b145085d6d5721449736e
Jeeeann <jdhu@ucdavis.edu>
2024-03-05 22:31:28 -0800

    Finish AudioManager and have a bgm worked

---
+++ Hollow/Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Use InputManager in project setting
+public class PlayerMovement : MonoBehaviour
+{
+    // Movement
+    public float moveSpeed;
+    Rigidbody2D rb;
+    Vector2 moveDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Frame rate dependent
+    void Update()
+    {
+        InputManagement();
+    }
+
+    // Frame rate independent
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputManagement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+}
