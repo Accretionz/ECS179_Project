fd9a65517b374944968a7347ca570f22a6344d25
MaggieK <makung0826@gmail.com>
2024-03-06 16:29:32 -0800

    sound added

---
+++ Hollow/Assets/Scripts/Sound.cs
@@ -0,0 +1,21 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 1.0f;
+    [Range(.1f, 3f)]
+    public float pitch = 1.0f;
+
+    public bool loop = true;
+
+    // Make it unshown
+    [HideInInspector]
+    public AudioSource source;
+}
